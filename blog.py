3c6754ab6aa51653ce22c1604bb17431a351116e
eswizardry <edrit011@gmail.com>
2016-09-16 14:53:13 +0700

    Update with blog feature and little CSS styles

--- blog.py
+++ blog.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 # @Author: mcsbanch
 # @Date:   2016-08-25 11:25:53
-# @Last Modified by:   mcsbanch
-# @Last Modified time: 2016-08-25 12:56:21
+# @Last Modified by:   eswizardry
+# @Last Modified time: 2016-09-16 14:26:56
 
 # blog.py - controller
 # import
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
+from functools import wraps
 import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+
 
 app = Flask(__name__)
 
@@ -24,14 +29,63 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
-@app.route("/")
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route("/", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All friends are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('Insert into posts (title, post) values(?, ?)', [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 
 @app.route("/main")
+@login_required
 def main():
-    return render_template("main.html")
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template("main.html", posts=posts)
 
 
 if __name__ == '__main__':
